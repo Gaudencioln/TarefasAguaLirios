84a2a53c894c0c93e520f8fbffec8b6a6b274f47
Gaudencioln <gaudencioln@hotmail.com>
2025-10-01 13:59:50 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,26 @@
-const CACHE = 'al-tarefas-v21';
+const CACHE_NAME = 'al-tarefas-v2.1.2';
 const ASSETS = [
   './',
   './index.html',
   './style.css',
   './app.js',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icon-72.png',
+  './icon-96.png',
+  './icon-144.png',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-self.addEventListener('install', (e)=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
 });
-self.addEventListener('activate', (e)=>{
-  e.waitUntil(self.clients.claim());
-});
-self.addEventListener('fetch', (e)=>{
-  e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request).catch(()=>caches.match('./index.html')))
+self.addEventListener('activate', e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))
   );
+  self.clients.claim();
+});
+self.addEventListener('fetch', e=>{
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
 });
